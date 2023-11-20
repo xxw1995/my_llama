f8e64ba0ad5189bb351a40de38237fe30188927f
xxw <46432007+xxw1995@users.noreply.github.com>
2023-11-20 15:26:38 +0800

    Update my_easy_llama.py

--- my_easy_llama.py
+++ my_easy_llama.py
@@ -15,7 +15,6 @@ from transformers.modeling_utils import PreTrainedModel
 from transformers.models.llama.configuration_llama import LlamaConfig
 
 def rotate_half(x):
-   """Rotates half the hidden dims of the input."""
    x1 = x[..., : x.shape[-1] // 2]
    x2 = x[..., x.shape[-1] // 2 :]
    return torch.cat((-x2, x1), dim=-1)
